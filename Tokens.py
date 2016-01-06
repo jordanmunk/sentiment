0987adfcb61ce4e1125cd1add0f17e658e1f0158
Anton <Antons@live.nl>
2016-01-06 11:55:10 +0100

    Hide tokens

--- Tokens.py
+++
@@ -1,6 +0,0 @@
-#Contains the tokens needed for access to twitter API
-consumer_key = 'gVtLceYtTRhbygl5TJA8zgK2m'
-consumer_secret = 'ErQRdZBW4KxI2aIcENqQZNubp3KLNr7kcL0NfaB8iMUNQ5iz1u'
-access_token = '4285110616-F31Ne5U5r8IGcOouJKvZe2SynlDwDPkEbtYrdtz'
-access_secret = 'Mdy0gx8GOoySCGsV6WtW6NSCpJQ9RkbwYIwxcAcVHkNXB'
-
